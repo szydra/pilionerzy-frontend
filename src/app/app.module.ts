260dfc57088477bfe70b660aeb99fe5f5f469fb1
szydra <szymon.draga@gmail.com>
2018-01-06 11:53:31 +0100

    Update app.module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GameComponent } from './components/game/game.component';
+import { QuestionComponent } from './components/question/question.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/nav-bar/nav-bar.component';
+import { SpinnerComponent } from './components/spinner/spinner.component';
+
+import { GameService } from './services/game.service';
+import { GameUiService } from './services/game-ui.service';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionComponent,
+    GameComponent,
+    HomeComponent,
+    NavbarComponent,
+    SpinnerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    GameService,
+    GameUiService
+  ],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
