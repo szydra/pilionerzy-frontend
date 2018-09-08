9fe1dac6a459390c4c7874b1d00aa28ad0bf4b19
szydra <szymon.draga@gmail.com>
2018-09-08 20:39:39 +0200

    Rest endpoint can be set globally.

---
+++ src/app/config.ts
@@ -0,0 +1,3 @@
+'use strict';
+
+export const REST_ENDPOINT = 'http://localhost:8080';
--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Question } from '../models/question';
+import * as config from '../config';
 
 import 'rxjs/add/operator/map';
 
@@ -10,9 +11,11 @@ export class GameService {
   constructor(private http: HttpClient) { }
 
   sendAnswer(selected: string): Promise<string> {
-    let url = "http://localhost:8080/answer/send/" + localStorage.getItem("gameId");
+    let url = config.REST_ENDPOINT + "/answer/send/"
+      + localStorage.getItem("gameId");
     let headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-    return this.http.post(url, JSON.stringify({ "selected": selected }), { headers: headers })
+    return this.http.post(url, JSON.stringify({ "selected": selected }),
+      { headers: headers })
       .toPromise()
       .then(res => {
         let answer = res['prefix'];
@@ -24,14 +27,15 @@ export class GameService {
   }
 
   startNewGame(): Promise<void> {
-    let url = "http://localhost:8080/game/start/";
+    let url = config.REST_ENDPOINT + "/game/start/";
     return this.http.get(url).toPromise().then(
       game => localStorage.setItem("gameId", game["id"])
     );
   }
 
   stopGame(): Promise<string> {
-    let url = "http://localhost:8080/game/stop/" + localStorage.getItem("gameId");
+    let url = config.REST_ENDPOINT + "/game/stop/"
+      + localStorage.getItem("gameId");
     return this.http.get(url).toPromise().then(res => {
       localStorage.removeItem("gameId");
       return res["prefix"];
@@ -39,7 +43,7 @@ export class GameService {
   }
 
   getQuestion(): Promise<Question> {
-    let url = "http://localhost:8080/question/get/";
+    let url = config.REST_ENDPOINT + "/question/get/";
     let gameId = localStorage.getItem("gameId");
     url += gameId ? gameId : 0;
     return this.http.get(url).map(res => <Question>res).toPromise();
