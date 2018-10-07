db41f590625b8e82736cfc7a6e2d2dd752ef54d5
szydra <szymon.draga@gmail.com>
2018-10-07 22:01:25 +0200

    New modules registered.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { GameComponent } from './components/game/game.component';
 import { HomeComponent } from './components/home/home.component';
+import { NewQuestion } from './components/new-question/new-question.component';
 
 const routes: Routes = [
   { path: 'game', component: GameComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'newquestion', component: NewQuestion },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', redirectTo: '/home', pathMatch: 'full' }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,50 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
+import { AlertCloseable } from './components/alert-closeable/alert-closeable';
 import { AppComponent } from './app.component';
 import { GameComponent } from './components/game/game.component';
 import { QuestionComponent } from './components/question/question.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/nav-bar/nav-bar.component';
 import { SpinnerComponent } from './components/spinner/spinner.component';
+import { NewQuestion } from './components/new-question/new-question.component';
+import { TextareaPreview } from './components/textarea-preview/textarea-preview';
 
 import { GameService } from './services/game.service';
 import { GameUiService } from './services/game-ui.service';
+import { QuestionService } from './services/question.service';
 import { AppRoutingModule } from './app-routing.module';
 
 import { MathJaxDirective } from './directives/mathjax.directive';
 
 @NgModule({
   declarations: [
+    AlertCloseable,
     AppComponent,
     QuestionComponent,
     GameComponent,
     HomeComponent,
     NavbarComponent,
     SpinnerComponent,
+    NewQuestion,
+    TextareaPreview,
     MathJaxDirective
   ],
   imports: [
     AppRoutingModule,
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    NgbModule
   ],
   providers: [
     GameService,
-    GameUiService
+    GameUiService,
+    QuestionService
   ],
   bootstrap: [AppComponent]
 })
