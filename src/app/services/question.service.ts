e75fffa30dc6447f3d3763606fbaf1282df4dc5c
szydra <szymon.draga@gmail.com>
2018-10-17 19:36:18 +0200

    Question service added.

---
+++ src/app/services/question.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Question } from '../models/question';
+
+import * as config from '../config';
+
+@Injectable()
+export class QuestionService {
+
+  constructor(private http: HttpClient) { }
+
+  addQuestion(question: Question): Promise<any> {
+    let url = config.REST_ENDPOINT + "/question/add";
+    let headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.post(url, JSON.stringify(question), { headers: headers })
+      .toPromise();
+  }
+
+}
