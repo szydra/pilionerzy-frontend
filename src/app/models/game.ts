05591b32aeb884d2debc31a27627cfa8d3b807d5
szydra <szymon.draga@gmail.com>
2019-03-03 17:46:12 +0100

    Apply Linter rules to models

--- src/app/models/game.ts
+++ src/app/models/game.ts
@@ -1,12 +1,12 @@
-import { Lifeline } from './lifeline';
-import { Question } from './question';
+import {Lifeline} from './lifeline';
+import {Question} from './question';
 
 export class Game {
   public static readonly HIGHEST_LEVEL: number = 12;
-  public static readonly GUARANTED_LEVELS: number[] = [12, 7, 2, 0];
-  public static readonly AWARDS: string[] = ["500 zł", "1 000 zł",
-    "2 000 zł", "5 000 zł", "10 000 zł", "20 000 zł", "40 000 zł",
-    "75 000 zł", "125 000 zł", "250 000 zł", "500 000 zł", "1 000 000 zł"];
+  public static readonly GUARANTEED_LEVELS: number[] = [12, 7, 2, 0];
+  public static readonly AWARDS: string[] = ['500 zł', '1 000 zł',
+    '2 000 zł', '5 000 zł', '10 000 zł', '20 000 zł', '40 000 zł',
+    '75 000 zł', '125 000 zł', '250 000 zł', '500 000 zł', '1 000 000 zł'];
 
   constructor() {
     this.level = 0;
--- src/app/models/question.ts
+++ src/app/models/question.ts
@@ -1,14 +1,14 @@
-import { SingleAnswer } from './single-answer';
+import {SingleAnswer} from './single-answer';
 
 export class Question {
   static readonly PREFIXES: string[] = ['A', 'B', 'C', 'D'];
 
-  content: string = '';
+  content = '';
   answers: SingleAnswer[];
   correctAnswer: string;
 
   constructor() {
-    let answers = new Array<SingleAnswer>();
+    const answers = new Array<SingleAnswer>();
     Question.PREFIXES.forEach(prefix => answers.push(new SingleAnswer(prefix, '')));
     this.answers = answers;
   }
