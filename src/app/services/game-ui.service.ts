77f6988df12be890e42cf09051aaf86c514486b5
szydra <szymon.draga@gmail.com>
2019-11-19 20:07:52 +0100

    Remove unused methods

--- src/app/services/game-ui.service.ts
+++ src/app/services/game-ui.service.ts
@@ -6,12 +6,6 @@ export class GameUiService {
   constructor() {
   }
 
-  // This method returns the height of the highest matched element.
-  // https://stackoverflow.com/a/6061029/8701267
-  private static getHeight(elements: HTMLCollection): number {
-    return Math.max.apply(null, Array.from(elements).map(element => element.clientHeight));
-  }
-
   disableHover(): void {
     setTimeout(() => {
       Array.from(document.querySelectorAll('.bg-unselected, .bg-selected'))
@@ -28,17 +22,6 @@ export class GameUiService {
           element.classList.remove('disable-hover');
         });
     });
-  }
-
-  private resizeFont(elements: HTMLCollection, maxHeight: number): void {
-    let currentHeight = GameUiService.getHeight(elements);
-    while (currentHeight > maxHeight) {
-      Array.from(elements).forEach(function (element) {
-        const style = (<HTMLElement>element).style;
-        style.fontSize = parseFloat(style.fontSize) - 1 + 'px';
-      });
-      currentHeight = GameUiService.getHeight(elements);
-    }
   }
 
   resetLevelBoxCorners(): void {
@@ -59,17 +42,6 @@ export class GameUiService {
         element.style.borderTopLeftRadius = '10px';
         element.style.borderTopRightRadius = '10px';
       }
-    });
-  }
-
-  updateFontSize(): void {
-    setTimeout(() => {
-      Array.from(document.querySelectorAll('.answer, .question'))
-        .forEach(function (element) {
-          (<HTMLElement>element).style.fontSize = '20px';
-        });
-      this.resizeFont(document.getElementsByClassName('answer'), 80);
-      this.resizeFont(document.getElementsByClassName('question'), 90);
     });
   }
 }
