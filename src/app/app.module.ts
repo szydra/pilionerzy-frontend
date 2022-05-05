a995f285708986da4be7a69ecb2fdd951984e08e
szydra <szymon.draga@gmail.com>
2022-05-05 20:25:26 +0200

    Move spinner to shared and fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,8 @@ import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import {NgModule} from '@angular/core';
 
-import {NgbAlertModule, NgbPopoverModule} from '@ng-bootstrap/ng-bootstrap';
+import {NgbPopoverModule} from '@ng-bootstrap/ng-bootstrap';
 
-import {AlertCloseableComponent} from './components/alert-closeable/alert-closeable.component';
 import {AppComponent} from './app.component';
 import {AskTheAudienceComponent} from './components/ask-the-audience/ask-the-audience.component';
 import {GameComponent} from './components/game/game.component';
@@ -12,28 +11,24 @@ import {InfoPopoverComponent} from './components/info-popover/info-popover.compo
 import {NewQuestionComponent} from './components/new-question/new-question.component';
 import {PhoneAFriendComponent} from './components/phone-a-friend/phone-a-friend.component';
 import {QuestionComponent} from './components/question/question.component';
-import {SpinnerComponent} from './components/spinner/spinner.component';
 import {TextareaPreviewComponent} from './components/textarea-preview/textarea-preview.component';
 
-import {MathJaxDirective} from './directives/mathjax.directive';
 import {MaxHeightDirective} from './directives/max-height.directive';
 import {AppCommonModule} from './app-common/app-common.module';
 import {AppRoutingModule} from './app-routing.module';
 import {CoreModule} from '@core/core.module';
+import {SharedModule} from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AlertCloseableComponent,
     AppComponent,
     AskTheAudienceComponent,
     GameComponent,
     InfoPopoverComponent,
-    MathJaxDirective,
     MaxHeightDirective,
     NewQuestionComponent,
     PhoneAFriendComponent,
     QuestionComponent,
-    SpinnerComponent,
     TextareaPreviewComponent
   ],
   imports: [
@@ -42,8 +37,9 @@ import {CoreModule} from '@core/core.module';
     BrowserModule,
     CoreModule,
     FormsModule,
-    NgbAlertModule,
-    NgbPopoverModule
+    NgbPopoverModule,
+    // TODO Remove
+    SharedModule
   ],
   bootstrap: [
     AppComponent
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,22 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MathJaxDirective} from './directives/mathjax.directive';
 import {AlertCloseableComponent} from './components/alert-closeable/alert-closeable.component';
+import {NgbAlertModule} from '@ng-bootstrap/ng-bootstrap';
+import {SpinnerComponent} from './components/spinner/spinner.component';
 
 @NgModule({
   declarations: [
     AlertCloseableComponent,
+    SpinnerComponent,
     MathJaxDirective
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    NgbAlertModule
   ],
   exports: [
     AlertCloseableComponent,
+    SpinnerComponent,
     MathJaxDirective
   ]
 })
