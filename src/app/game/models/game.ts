06631ee68bf74915074586be6c53088f1726239d
szydra <szymon.draga@gmail.com>
2022-09-16 19:24:59 +0200

    Remove hard-coded values from game model

--- src/app/game/models/game.ts
+++ src/app/game/models/game.ts
@@ -2,12 +2,6 @@ import {Lifeline} from './lifeline';
 import {Question} from '@core/models/question';
 
 export class Game {
-  public static readonly HIGHEST_LEVEL: number = 12;
-  public static readonly GUARANTEED_LEVELS: number[] = [12, 7, 2, 0];
-  public static readonly AWARDS: string[] = ['500 zł', '1 000 zł',
-    '2 000 zł', '5 000 zł', '10 000 zł', '20 000 zł', '40 000 zł',
-    '75 000 zł', '125 000 zł', '250 000 zł', '500 000 zł', '1 000 000 zł'];
-
   constructor() {
     this.level = 0;
     this.active = true;
@@ -18,8 +12,4 @@ export class Game {
   correctAnswer: string;
   lastQuestion: Question;
   usedLifelines: Lifeline[] = [];
-
-  get awards(): string[] {
-    return Game.AWARDS;
-  }
 }
